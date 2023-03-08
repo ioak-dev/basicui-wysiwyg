20ee9b07d4c97b0f966e2e6f03932a8d8e9f0251
Arunkumar Selvaraj <arun@ioak.io>
2023-03-08 12:18:25 +0530

    base version

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./style.css";
+
+export interface EditorProps {
+};
+
+/**
+ * Component to render drop down input form element. Supports multi select and auto complete features
+ */
+const Editor = (props: EditorProps) => {
+    return (
+        <div>
+            test
+        </div>
+    );
+};
+
+export default Editor;
